e59bdd3f498e4ea82dc9196dcc8f1bc069074f00
Nicolas Mariot <nicolas.mariot@e-nigma.fr>
2016-02-15 19:32:01 +0100

    Initial commit

---
+++ WordGeneration/InternalGeneration.cs
@@ -0,0 +1,170 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WordGeneration
+{
+    /// <summary>
+    /// Internal generation
+    /// </summary>
+    class InternalGeneration
+    {
+        static readonly string[] WORDS = { "a", "ac", "adipiscing", "aenean", "aliquam", "aliquet", "amet", "ante", "arcu", "at", "augue", "bibendum", "blandit", "congue", "consectetuer", "consectetur", "consequat", "convallis", "cras", "cubilia", "curabitur", "curae;", "cursus", "diam", "dignissim", "dolor", "dui", "duis", "egestas", "eleifend", "elementum", "elit", "enim", "erat", "eros", "est", "et", "eu", "euismod", "faucibus", "felis", "fermentum", "feugiat", "fusce", "gravida", "hendrerit", "iaculis", "id", "imperdiet", "in", "integer", "ipsum", "justo", "lacus", "lectus", "leo", "libero", "ligula", "lobortis", "lorem", "luctus", "maecenas", "magna", "massa", "mauris", "metus", "mi", "molestie", "mollis", "morbi", "nec", "nibh", "nisl", "non", "nonummy", "nulla", "nunc", "odio", "orci", "ornare", "pede", "pellentesque", "pharetra", "placerat", "porttitor", "posuere", "praesent", "pretium", "primis", "proin", "pulvinar", "purus", "quam", "quis", "rhoncus", "risus", "rutrum", "sapien", "scelerisque", "sed", "sem", "semper", "sit", "sodales", "sollicitudin", "suscipit", "suspendisse", "tellus", "tempor", "tempus", "tincidunt", "tortor", "tristique", "turpis", "ullamcorper", "ultrices", "ultricies", "ut", "varius", "vehicula", "vel", "velit", "vestibulum", "vitae", "vivamus", "volutpat", "vulputate" };
+
+        /// <summary>
+        /// Progress of the generation
+        /// </summary>
+        public event EventHandler<DocGenerationEventArgs> Progress;
+
+        /// <summary>
+        /// Total number of documents
+        /// </summary>
+        public long NbTotalDocs { get; private set; }
+
+        /// <summary>
+        /// Destination folder
+        /// </summary>
+        public string DestFolder { get; private set; }
+
+        /// <summary>
+        /// Number of files per folder
+        /// </summary>
+        public int NbFilesPerFolder { get; private set; }
+
+        /// <summary>
+        /// Number of folders per folder
+        /// </summary>
+        public int NbFoldersPerFolder { get; private set; }
+
+        /// <summary>
+        /// Number of paragraphs per document
+        /// </summary>
+        public int NbParagraphesPerDoc { get; private set; }
+
+        /// <summary>
+        /// Number of words per paragraphs
+        /// </summary>
+        public int NbWordsPerParagraph { get; private set; }
+
+        private int _depth;
+
+        #region Random management in multithread context       
+        private static Random _global = new Random();
+        [ThreadStatic]
+        private static Random _local;
+
+        /// <summary>
+        /// Return a random word
+        /// </summary>
+        /// <returns>A work chosen into the array</returns>
+        private string GetRandomWord()
+        {
+            Random inst = _local;
+            if (inst == null)
+            {
+                int seed;
+                lock (_global) seed = _global.Next();
+                _local = inst = new Random(seed);
+            }
+
+            return WORDS[inst.Next(0, WORDS.Length - 1)];
+        }
+        #endregion
+
+        private int _nbDocsGenerated = 0;
+
+        /// <summary>
+        /// Get an instance of the Generation class
+        /// </summary>
+        /// <param name="nbTotalDocs"></param>
+        /// <param name="destFolder">Destination folder</param>
+        /// <param name="nbFilesPerFolder">Number of files per folder</param>
+        /// <param name="nbFoldersPerFolder">Number of folders per folder</param>
+        /// <param name="nbParaPerDoc">Number of paragraphs per document</param>
+        /// <param name="nbWordsPerPara">Number of words per paragraph</param>
+        public InternalGeneration(long nbTotalDocs, string destFolder, int nbFilesPerFolder, int nbFoldersPerFolder, int nbParaPerDoc, int nbWordsPerPara)
+        {
+            this.NbTotalDocs = nbTotalDocs;
+            this.DestFolder = destFolder;
+            this.NbFilesPerFolder = nbFilesPerFolder;
+            this.NbFoldersPerFolder = nbFoldersPerFolder;
+            this.NbParagraphesPerDoc = nbParaPerDoc;
+            this.NbWordsPerParagraph = nbWordsPerPara;
+
+            // Calculate the depth of our folders
+            for (_depth = 0; ; _depth++)
+            {
+                if (nbTotalDocs <= this.NbFilesPerFolder * Math.Pow(this.NbFoldersPerFolder, _depth))
+                {
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Launch the generation with parameters specified in the constructor
+        /// </summary>
+        public void Launch()
+        {
+            _nbDocsGenerated = 0;
+            Parallel.For(_nbDocsGenerated, this.NbTotalDocs, GenerateDocument);
+        }
+
+        /// <summary>
+        /// Generate a Word document
+        /// </summary>
+        /// <param name="number">Document number</param>
+        public void GenerateDocument(long number)
+        {
+            // Evaluate final path
+            string finalPath = this.DestFolder;
+            for (int folder = 0; folder < _depth; folder++)
+            {
+                long n = (long)Math.Pow(this.NbFoldersPerFolder, _depth - folder - 1) * this.NbFoldersPerFolder;
+                finalPath = Path.Combine(finalPath, "Folder" + (number / n).ToString());
+            }
+            Directory.CreateDirectory(finalPath);
+
+            string path = Path.Combine(finalPath, $"{GetRandomWord()} {GetRandomWord()} {GetRandomWord()} {number.ToString()}.docx");
+
+            // Writing in the word document
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(path, DocumentFormat.OpenXml.WordprocessingDocumentType.Document, true))
+            {
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                for (var numPara = 0; numPara < this.NbParagraphesPerDoc; numPara++)
+                {
+                    Paragraph para = body.AppendChild(new Paragraph());
+                    Run run = para.AppendChild(new Run());
+                    StringBuilder sb = new StringBuilder();
+                    for (var numWord = 0; numWord < this.NbWordsPerParagraph; numWord++)
+                    {
+                        sb.Append(GetRandomWord() + " ");
+                    }
+                    run.AppendChild(new Text(sb.ToString()));
+                }
+            }
+
+            // Raising the progress event
+            _nbDocsGenerated++;
+            OnProgress(_nbDocsGenerated, this.NbTotalDocs);
+        }
+
+
+
+        private void OnProgress(long current, long nbDocs)
+        {
+            if (Progress != null)
+            {
+                Progress(this, new DocGenerationEventArgs() { DocsGenerated = current, NumberOfDocs = nbDocs });
+            }
+        }
+    }
+}
---
+++ WordGeneration/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WordGeneration
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string DEFAULT_FOLDER = "docs";
+
+        private Stopwatch _sw;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set default values
+            txtGenerationFolder.Text = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), DEFAULT_FOLDER);
+            pbProgress.Value = 0;
+        }
+
+        async private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            bool parsed = true;
+            long nbDocs;
+            int nbFilesPerFolder, nbFoldersPerFolder;
+            parsed &= long.TryParse(txtNbDocs.Text, out nbDocs);
+            parsed &= int.TryParse(txtFilesPerFolder.Text, out nbFilesPerFolder);
+            parsed &= int.TryParse(txtFoldersPerFolder.Text, out nbFoldersPerFolder);
+
+            if (!parsed)
+            {
+                System.Windows.MessageBox.Show(this, "Error while parsing values : check your string formats", "Word Generation", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                var gen = new DocGeneration();
+                gen.Progress += Gen_Progress;
+                btnGo.IsEnabled = false;
+
+                _sw = Stopwatch.StartNew();
+                await gen.Generate(nbDocs, txtGenerationFolder.Text, nbFilesPerFolder, nbFoldersPerFolder, (int)sldParaPerDoc.Value, (int)sldWordsPerPara.Value);
+                pbProgress.Value = 100;
+                btnGo.IsEnabled = true;
+                Process.Start(txtGenerationFolder.Text);
+                System.Windows.MessageBox.Show(this, $"{nbDocs} documents generated in {_sw.ElapsedMilliseconds} ms", "Word Generation", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+
+
+        private void Gen_Progress(object sender, DocGenerationEventArgs e)
+        {
+            // Update progress bar
+            Dispatcher.BeginInvoke(new Action(() => pbProgress.Value = (int)(e.DocsGenerated * 100 / e.NumberOfDocs)));
+        }
+
+        private void btnGenerationFolder_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dlg = new FolderBrowserDialog())
+            {
+                dlg.Description = "Choose the folder where the documents will be generated";
+                if (!string.IsNullOrWhiteSpace(txtGenerationFolder.Text)) dlg.SelectedPath = txtGenerationFolder.Text;
+                if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    txtGenerationFolder.Text = dlg.SelectedPath;
+                }
+            }
+        }
+    }
+}
