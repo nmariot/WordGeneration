cd8c18b5c44e69edbfb62c27ccc133bccc88c2b7
Nicolas Mariot <nicolas.mariot@e-nigma.fr>
2016-03-01 12:29:22 +0100

    Add missing files

---
+++ WordGeneration/DocGeneration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WordGeneration
+{
+    /// <summary>
+    /// Class used to generate n word documents
+    /// Its role is only to call InternalGeneration asynchronously and 
+    /// </summary>
+    class DocGeneration
+    {
+        /// <summary>
+        /// Progress of the generation
+        /// </summary>
+        public event EventHandler<DocGenerationEventArgs> Progress;
+
+        /// <summary>
+        /// Generate documents
+        /// </summary>
+        /// <param name="nbDocs">The number of documents to generate</param>
+        /// <param name="destFolder">Destination folder</param>
+        /// <param name="nbFilesPerFolder">Number of files per folder</param>
+        /// <param name="nbFoldersPerFolder">Number of folders per folder</param>
+        /// <param name="nbParaPerDoc">Number of paragraphs per document</param>
+        /// <param name="nbWordsPerPara">Number of words per paragraph</param>
+        public async Task Generate(long nbDocs, string destFolder, int nbFilesPerFolder = 1000, int nbFoldersPerFolder = 100, int nbParaPerDoc = 10, int nbWordsPerPara = 50)
+        {
+            var gen = new InternalGeneration(nbDocs, destFolder, nbFilesPerFolder, nbFoldersPerFolder, nbParaPerDoc, nbWordsPerPara);
+            gen.Progress += Gen_Progress;
+            await Task.Factory.StartNew(() =>
+            {
+                gen.Launch();
+            });
+        }
+
+        private void Gen_Progress(object sender, DocGenerationEventArgs e)
+        {
+            if (Progress != null)
+            {
+                Progress(this, new DocGenerationEventArgs() { DocsGenerated = e.DocsGenerated, NumberOfDocs = e.NumberOfDocs });
+            }
+        }
+    }
+}
---
+++ WordGeneration/DocGenerationEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordGeneration
+{
+    class DocGenerationEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Number of documents
+        /// </summary>
+        public long NumberOfDocs { get; set; }
+
+        /// <summary>
+        /// Number of generated docs
+        /// </summary>
+        public long DocsGenerated { get; set; }
+    }
+}
